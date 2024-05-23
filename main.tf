1f8a2bd8c3ca5d436da509f31b5d29c89b237f80
paketeserrano <pacoserrano@gmail.com>
2024-05-23 11:51:29 -0400

    Changing the aws profile

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 
    tags = {                                
      Name = "demo-server1"
-}
+  }
 }
 
 
--- providers.tf
+++ providers.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "default"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west1"
   region = "us-west-1"
-  profile = "Helen"
+  profile = "default"
 }
 
